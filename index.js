380bfc9f41f8ac822baec9d93aa8e0e5c9a877ee
Leon <hq_li1982@hotmail.com>
2021-07-11 19:19:59 +0800

    change mongoDB connection

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
   res.send('SERVER IS RUNNING');
 })
 
-// const CONNECTION_URL = 'mongodb+srv://memoryadmin:K7mJRz7R9p4HCSvA@cluster0.jvk8v.mongodb.net/memories?retryWrites=true&w=majority';
+
 
 const PORT = process.env.PORT || 5000;
 
